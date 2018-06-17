ddaf99b5baa325bbfcdfad7a4631528296f58ed7
fun-redoc <fun.redoc@gmail.com>
2018-06-17 21:37:34 +0200

    Initial Commit

---
+++ LittleAudioPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LittleAudioPlayer
+//
+//  Created by roland1 on 17.06.18.
+//  Copyright © 2018 rsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
